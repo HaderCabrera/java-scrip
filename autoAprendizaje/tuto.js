468bf2d2e64261567217929de51f75ac01edcfe0
juandgg <juanguiza65@gmail.com>
2023-11-29 15:28:26 -0500

    estuio java

---
+++ autoAprendizaje/tuto.js
@@ -0,0 +1,27 @@
+//DEFINIR VARIABLES
+var name = "jhon"
+//DEFINIR CONSTANTES
+const VELOCIDAD = 9.936;
+//MOSTRAR ALGO EN CONSOLA
+console.log(VELOCIDAD)
+//MOSTRAR EN EL DOCUMENTO
+document.write("Hola Hola")
+
+//OEPRADORES JAVASCRIPT
+let numberOne = 60;
+let numberTwo = 110;
+//Operar Numeros
+//let result = numberOne * numberTwo;
+//let result = numberOne > numberTwo;
+//let result = numberOne == numberTwo;
+let result = numberOne != numberTwo;
+if (result === true){
+    console.log("El número si no es el mismo")
+}
+console.log(result)
+//Operar string
+let names = "Jhon"
+let lastname = "Carter"
+
+let completeName = name + ' ' + lastname;
+console.log(completeName)
